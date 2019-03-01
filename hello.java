879f4fa1da9b400453b97cb03f57350b7d4f6d77
todo82 <todo82@hotmail.com>
2019-03-01 23:37:48 +0100

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
-public class Hello {
+public class vezbanje {
 public static void main (String[] args) {
 //Prints Hello world in console window
 system.out.println("Hello world is printed in console window as an example");
